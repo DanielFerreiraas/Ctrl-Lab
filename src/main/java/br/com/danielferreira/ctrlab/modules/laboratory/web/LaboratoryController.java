8acfd17280631e0ecc560710eb14198f09e167cb
Daniel Ferreira <daniel.ferreirass.dev@gmail.com>
2023-12-27 00:34:00 -0300

    feat: Melhorando os métodos do CRUD

--- src/main/java/br/com/danielferreira/ctrlab/modules/laboratory/web/LaboratoryController.java
+++ src/main/java/br/com/danielferreira/ctrlab/modules/laboratory/web/LaboratoryController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,11 +23,12 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("/laboratory")
 public class LaboratoryController {
+  
   @Autowired
   private LaboratoryService laboratoryService;
 
   @PostMapping
-  ResponseEntity<List<Laboratory>> create(@Valid @RequestBody Laboratory laboratory) {
+  ResponseEntity<Laboratory> create(@Valid @RequestBody Laboratory laboratory) {
     return ResponseEntity.status(HttpStatus.CREATED)
         .body(laboratoryService.create(laboratory));
   }
@@ -42,12 +44,12 @@ public class LaboratoryController {
   }
 
   @PutMapping("{id}")
-  List<Laboratory> update(@PathVariable String id, @RequestBody Laboratory laboratory) {
-    return laboratoryService.update(id, laboratory);
+  ResponseEntity<Laboratory> update(@PathVariable String id, @RequestBody Laboratory laboratory) {
+    return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(laboratoryService.update(id, laboratory));
   }
 
   @DeleteMapping("{id}")
-  List<Laboratory> delete(@PathVariable String id) {
-    return laboratoryService.delete(id);
+  ResponseEntity<Laboratory> delete(@PathVariable String id) {
+    return ResponseEntity.status(HttpStatusCode.valueOf(204)).body(laboratoryService.delete(id));
   }
 }
